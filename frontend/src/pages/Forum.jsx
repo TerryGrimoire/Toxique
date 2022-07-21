1d2bec1f9e4fef62f6d4591e07d44ffd041d4322
Terry Grimoire <terry.grimoire@gmail.com>
2022-07-21 19:54:06 +0200

    putting an end to the parlons-en component and making it viable at least in front end

--- frontend/src/pages/Forum.jsx
+++ frontend/src/pages/Forum.jsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import forum from "../fakeData/forum";
 
 function Forum() {
   const { id } = useParams();
+  const [comment, setComment] = useState();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    forum[id].comments.push({
+      id: 0,
+      content: comment,
+    });
+  };
+
   return (
     <div className="container">
       <h1>Forum</h1>
-      <h2>{forum[id].title}</h2>
+      <div className="forum-question dark">
+        <h2>{forum[id].title}</h2>
+        <p>{forum[id].description}</p>
+      </div>
       <section>
-        {forum[id].comments.map((comment) => (
-          <p className="comment">{comment.content}</p>
+        {forum[id].comments.map((commentMap) => (
+          <p className="comment">{commentMap.content}</p>
         ))}
       </section>
 
-      <form action="">
-        <input type="text" className="forum-input" />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          className="forum-input"
+          onChange={(e) => setComment(e.target.value)}
+        />
+        <button type="submit" className="send">
+          ...
+        </button>
       </form>
     </div>
   );
