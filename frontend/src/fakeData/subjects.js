c56bac4ac506dd22bd29193034ef92c50e5def4c
Terry Grimoire <terry.grimoire@gmail.com>
2022-07-21 16:57:55 +0200

    creating the fake fake data for subjects table and adding it to a static js file

---
+++ frontend/src/fakeData/subjects.js
@@ -0,0 +1,46 @@
+const subjects = [
+  {
+    subject_id: 0,
+    title: "Je ne veux pas que ma copine avorte",
+    description:
+      "La situation est compliquée, je suis blablabla et je voudrais blablablabla et en plus blablablbla, qu'en pensez-vous ? ",
+  },
+  {
+    subject_id: 0,
+    title: "Je ne veux pas que ma copine avorte",
+    description:
+      "La situation est compliquée, je suis blablabla et je voudrais blablablabla et en plus blablablbla, qu'en pensez-vous ? ",
+  },
+  {
+    subject_id: 0,
+    title: "Je ne veux pas que ma copine avorte",
+    description:
+      "La situation est compliquée, je suis blablabla et je voudrais blablablabla et en plus blablablbla, qu'en pensez-vous ? ",
+  },
+  {
+    subject_id: 0,
+    title: "Je ne veux pas que ma copine avorte",
+    description:
+      "La situation est compliquée, je suis blablabla et je voudrais blablablabla et en plus blablablbla, qu'en pensez-vous ? ",
+  },
+  {
+    subject_id: 0,
+    title: "Je ne veux pas que ma copine avorte",
+    description:
+      "La situation est compliquée, je suis blablabla et je voudrais blablablabla et en plus blablablbla, qu'en pensez-vous ? ",
+  },
+  {
+    subject_id: 0,
+    title: "Je ne veux pas que ma copine avorte",
+    description:
+      "La situation est compliquée, je suis blablabla et je voudrais blablablabla et en plus blablablbla, qu'en pensez-vous ? ",
+  },
+  {
+    subject_id: 0,
+    title: "Je ne veux pas que ma copine avorte",
+    description:
+      "La situation est compliquée, je suis blablabla et je voudrais blablablabla et en plus blablablbla, qu'en pensez-vous ? ",
+  },
+];
+
+export default subjects;
