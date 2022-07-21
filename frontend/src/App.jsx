3797836396fa12d45bc66f905ab82f03aec06856
Terry Grimoire <terry.grimoire@gmail.com>
2022-07-21 12:02:12 +0200

    Adding tailwind + creating the header + footer + all basic pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
-import Home from "@pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Education from "./pages/Education";
+import ParlonsEn from "./pages/ParlonsEn";
+import Depistage from "./pages/Depistage";
+import Error404 from "./pages/Error404";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Education" element={<Education />} />
+        <Route path="/Parlons_en" element={<ParlonsEn />} />
+        <Route path="/Depistage" element={<Depistage />} />
+        <Route path="/ERROR404" element={<Error404 />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Depistage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Depistage() {
+  return <div>Depistage</div>;
+}
+
+export default Depistage;
